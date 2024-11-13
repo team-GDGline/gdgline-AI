84854d7ea581941476219e961338bdd330e59419
YJ <jyjin1206@gmail.com>
2024-11-14 05:42:29 +0900

    불필요한 파일 삭제

--- model_trainning/ultralytics/engine/model.py
+++ model_trainning/ultralytics/engine/model.py
@@ -553,53 +553,6 @@ class Model(nn.Module):
             self.predictor.set_prompts(prompts)
         return self.predictor.predict_cli(source=source) if is_cli else self.predictor(source=source, stream=stream)
 
-    def track(
-        self,
-        source: Union[str, Path, int, list, tuple, np.ndarray, torch.Tensor] = None,
-        stream: bool = False,
-        persist: bool = False,
-        **kwargs,
-    ) -> List[Results]:
-        """
-        Conducts object tracking on the specified input source using the registered trackers.
-
-        This method performs object tracking using the model's predictors and optionally registered trackers. It handles
-        various input sources such as file paths or video streams, and supports customization through keyword arguments.
-        The method registers trackers if not already present and can persist them between calls.
-
-        Args:
-            source (Union[str, Path, int, List, Tuple, np.ndarray, torch.Tensor], optional): Input source for object
-                tracking. Can be a file path, URL, or video stream.
-            stream (bool): If True, treats the input source as a continuous video stream. Defaults to False.
-            persist (bool): If True, persists trackers between different calls to this method. Defaults to False.
-            **kwargs (Any): Additional keyword arguments for configuring the tracking process.
-
-        Returns:
-            (List[ultralytics.engine.results.Results]): A list of tracking results, each a Results object.
-
-        Raises:
-            AttributeError: If the predictor does not have registered trackers.
-
-        Examples:
-            >>> model = YOLO("yolo11n.pt")
-            >>> results = model.track(source="path/to/video.mp4", show=True)
-            >>> for r in results:
-            ...     print(r.boxes.id)  # print tracking IDs
-
-        Notes:
-            - This method sets a default confidence threshold of 0.1 for ByteTrack-based tracking.
-            - The tracking mode is explicitly set in the keyword arguments.
-            - Batch size is set to 1 for tracking in videos.
-        """
-        if not hasattr(self.predictor, "trackers"):
-            from ultralytics.trackers import register_tracker
-
-            register_tracker(self, persist)
-        kwargs["conf"] = kwargs.get("conf") or 0.1  # ByteTrack-based method needs low confidence predictions as input
-        kwargs["batch"] = kwargs.get("batch") or 1  # batch-size 1 for tracking in videos
-        kwargs["mode"] = "track"
-        return self.predict(source=source, stream=stream, **kwargs)
-
     def val(
         self,
         validator=None,
